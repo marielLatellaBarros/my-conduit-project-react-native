579f1e2c95b444cdaab22c65b6f14fb9be6e2e9c
Mariel Latella Barros <mariel.latella@gmail.com>
2020-11-17 21:24:16 +0100

    Add static list of articles to feed the article preview component

---
+++ assets/articles.js
@@ -0,0 +1,278 @@
+export const ARTICLES =  [
+    {
+      title: 'React Native Course',
+      slug: 'test-e93rtl',
+      body: 'Welcome to the React Native Course! I hope you\'re enjoying the content!',
+      createdAt: '2019-10-06T09:56:06.330Z',
+      updatedAt: '2019-10-06T09:56:06.330Z',
+      tagList: ['test', 'jsx', 'react'],
+      description: 'Learn to build mobile applications with React Native',
+      author: {
+        username: 'be.samwolfs@gmail.com',
+        bio: null,
+        image: 'https://static.productionready.io/images/smiley-cyrus.jpg',
+        following: false
+      },
+      favorited: false,
+      favoritesCount: 0
+    },
+    {
+      title: 'sunday test article',
+      slug: 'sunday-test-article-1ek2ts',
+      body: 'sunday test article',
+      createdAt: '2019-10-06T08:24:19.125Z',
+      updatedAt: '2019-10-06T08:24:19.125Z',
+      tagList: [],
+      description: 'sunday test article',
+      author: { username: 'testingKha', bio: null, image: 'https://static.productionready.io/images/smiley-cyrus.jpg', following: false },
+      favorited: false,
+      favoritesCount: 1
+    },
+    {
+      title: 'sunday test',
+      slug: 'sunday-test-bdjlue',
+      body: 'testing create article',
+      createdAt: '2019-10-06T08:20:10.641Z',
+      updatedAt: '2019-10-06T08:20:10.641Z',
+      tagList: [],
+      description: 'testing create article',
+      author: { username: 'testingKha', bio: null, image: 'https://static.productionready.io/images/smiley-cyrus.jpg', following: false },
+      favorited: false,
+      favoritesCount: 0
+    },
+    {
+      title: 'FOda-se',
+      slug: 'foda-se-soabcu',
+      body: 'dkodkwokdw',
+      createdAt: '2019-10-06T05:54:04.939Z',
+      updatedAt: '2019-10-06T05:54:04.939Z',
+      tagList: [],
+      description: 'dopkodkwok',
+      author: {
+        username: 'Pedrinho12345rj',
+        bio: null,
+        image: 'https://static.productionready.io/images/smiley-cyrus.jpg',
+        following: false
+      },
+      favorited: false,
+      favoritesCount: 3
+    },
+    {
+      title: 'this is a test article',
+      slug: 'this-is-a-test-article-jtwybw',
+      body: 'testing 1 2 3 ',
+      createdAt: '2019-10-06T02:01:38.789Z',
+      updatedAt: '2019-10-06T02:01:38.789Z',
+      tagList: [],
+      description: 'absolutely nothing',
+      author: { username: 'lijlijlij', bio: null, image: 'https://static.productionready.io/images/smiley-cyrus.jpg', following: false },
+      favorited: false,
+      favoritesCount: 2
+    },
+    {
+      title: 'Conduit',
+      slug: 'conduit-dj7yj9',
+      body: 'article',
+      createdAt: '2019-10-05T23:08:45.465Z',
+      updatedAt: '2019-10-05T23:08:45.465Z',
+      tagList: [],
+      description: 'about',
+      author: { username: 'u20191004', bio: 'bio bio', image: 'https://i.stack.imgur.com/xHWG8.jpg', following: false },
+      favorited: false,
+      favoritesCount: 1
+    },
+    {
+      title: ' jk ',
+      slug: 'jk-49yul0',
+      body: ' k kj kj jk kj ',
+      createdAt: '2019-10-05T18:51:51.155Z',
+      updatedAt: '2019-10-05T18:51:51.155Z',
+      tagList: [],
+      description: ' k kkj jk jk jk',
+      author: {
+        username: 'my name 786786989',
+        bio: null,
+        image: 'https://static.productionready.io/images/smiley-cyrus.jpg',
+        following: false
+      },
+      favorited: false,
+      favoritesCount: 3
+    },
+    {
+      title: 'Kiskeam',
+      slug: 'kiskeam-vvw66r',
+      body: 'dsdfsdf',
+      createdAt: '2019-10-05T18:35:13.790Z',
+      updatedAt: '2019-10-05T18:35:13.790Z',
+      tagList: [],
+      description: 'sdfs',
+      author: { username: 'hellow123', bio: null, image: 'https://static.productionready.io/images/smiley-cyrus.jpg', following: false },
+      favorited: false,
+      favoritesCount: 4
+    },
+    {
+      title: 'hmmmm',
+      slug: 'hmmmm-ryvnfi',
+      body: 'best',
+      createdAt: '2019-10-05T18:34:30.954Z',
+      updatedAt: '2019-10-05T18:34:30.954Z',
+      tagList: [],
+      description: 'okay',
+      author: { username: 'm00zi', bio: null, image: 'https://static.productionready.io/images/smiley-cyrus.jpg', following: false },
+      favorited: false,
+      favoritesCount: 3
+    },
+    {
+      title: 'I am article title',
+      slug: 'i-am-article-title-9jqtvz',
+      body: 'MARKDOWN',
+      createdAt: '2019-10-05T18:07:42.957Z',
+      updatedAt: '2019-10-05T18:07:42.957Z',
+      tagList: [],
+      description: 'Everything is what this is about',
+      author: {
+        username: 'Test Man',
+        bio: 'I test things.',
+        image: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+        following: false
+      },
+      favorited: false,
+      favoritesCount: 1
+    },
+    {
+      title: 'testing articles',
+      slug: 'teste-v8bcj4',
+      body: 'teste',
+      createdAt: '2019-10-05T17:36:26.656Z',
+      updatedAt: '2019-10-05T17:40:56.972Z',
+      tagList: [],
+      description: 'teste',
+      author: { username: '_Jujubinha123', bio: null, image: 'https://static.productionready.io/images/smiley-cyrus.jpg', following: false },
+      favorited: false,
+      favoritesCount: 0
+    },
+    {
+      title: 'Thor the king of Aroma ancient',
+      slug: 'thor-the-king-of-aroma-ancient-b6g2fk',
+      body: 'Thor the demigod of thunder storm',
+      createdAt: '2019-10-05T17:00:31.835Z',
+      updatedAt: '2019-10-05T17:00:31.835Z',
+      tagList: [],
+      description: 'The liar thor',
+      author: {
+        username: 'glory232',
+        bio: 'narshimha ready',
+        image: 'https://avatoon.net/wp-content/uploads/2018/06/Avatoon-Blog-Cartoon-Avatar.jpg',
+        following: false
+      },
+      favorited: false,
+      favoritesCount: 2
+    },
+    {
+      title: 'A new Article',
+      slug: 'a-new-article-fsd18v',
+      body:
+        'Markdown Example\n===============\n\nThis is the raw markdown used to generate the below web content.\n\nBulleted List\n-------------\n\n* Foo\n* Bar\n\nNumbered List\n-------------\n\n1. Foo\n2. Bar\n\nFormatting\n----------\n\nCan be **bold** or *italic*\n\nLinks\n-----\n\nClick [here](http://foo.com) to go somewhere.',
+      createdAt: '2019-10-05T16:59:21.281Z',
+      updatedAt: '2019-10-05T16:59:21.281Z',
+      tagList: [],
+      description: 'something something something',
+      author: { username: 'ernestohs', bio: null, image: 'https://static.productionready.io/images/smiley-cyrus.jpg', following: false },
+      favorited: false,
+      favoritesCount: 1
+    },
+    {
+      title: 'This is test details',
+      slug: 'this-is-test-details-xon0p8',
+      body: 'Software Developer at Dexbytes Infotech Pvt. Ltd Indore',
+      createdAt: '2019-10-05T14:42:42.417Z',
+      updatedAt: '2019-10-05T14:42:42.417Z',
+      tagList: [],
+      description: 'New subject for publish',
+      author: {
+        username: 'Munna Prasad',
+        bio: null,
+        image: 'https://www.cambridgeenglish.org/Images/tye-business-button-2019.jpg',
+        following: false
+      },
+      favorited: false,
+      favoritesCount: 2
+    },
+    {
+      title: 'fs',
+      slug: 'fs-bcb8gh',
+      body: 'fr',
+      createdAt: '2019-10-05T12:42:18.085Z',
+      updatedAt: '2019-10-05T12:42:18.085Z',
+      tagList: [],
+      description: 'sf',
+      author: { username: 'hannah', bio: null, image: 'https://static.productionready.io/images/smiley-cyrus.jpg', following: false },
+      favorited: false,
+      favoritesCount: 0
+    },
+    {
+      title: 'asf',
+      slug: 'asf-h2ujbx',
+      body: 'sg',
+      createdAt: '2019-10-05T12:41:44.407Z',
+      updatedAt: '2019-10-05T12:41:44.407Z',
+      tagList: [],
+      description: 'sf',
+      author: { username: 'hannah', bio: null, image: 'https://static.productionready.io/images/smiley-cyrus.jpg', following: false },
+      favorited: false,
+      favoritesCount: 1
+    },
+    {
+      title: 'sdfgn',
+      slug: 'sdfgn-s4i3ph',
+      body: 'asf',
+      createdAt: '2019-10-05T12:38:59.313Z',
+      updatedAt: '2019-10-05T12:38:59.313Z',
+      tagList: [],
+      description: 'sdfgh',
+      author: { username: 'hannah', bio: null, image: 'https://static.productionready.io/images/smiley-cyrus.jpg', following: false },
+      favorited: false,
+      favoritesCount: 1
+    },
+    {
+      title: "Wow, this's a nice example",
+      slug: 'wow-this-s-a-nice-example-qcte2m',
+      body: "I'm Jun from Vietnam and I'm learning Angular for Jobs and make some ideas into real for helping everyone around me. ",
+      createdAt: '2019-10-05T12:14:06.422Z',
+      updatedAt: '2019-10-05T12:14:06.422Z',
+      tagList: ['learning', 'Vietnamese developer'],
+      description: 'How did i find this example?',
+      author: { username: 'kyris', bio: null, image: 'https://static.productionready.io/images/smiley-cyrus.jpg', following: false },
+      favorited: false,
+      favoritesCount: 1
+    },
+    {
+      title: 'Test',
+      slug: 'test-ew2q2q',
+      body: 'test',
+      createdAt: '2019-10-05T10:53:09.045Z',
+      updatedAt: '2019-10-05T10:53:09.045Z',
+      tagList: [],
+      description: 'test',
+      author: {
+        username: 'qaser',
+        bio: null,
+        image: 'https://www.biography.com/.image/t_share/MTE5NDg0MDU1MjM5ODg2MzUx/dwayne-johnson-11818916-1-402.jpg',
+        following: false
+      },
+      favorited: false,
+      favoritesCount: 2
+    },
+    {
+      title: 'kkm',
+      slug: 'kkm-fu263a',
+      body: 'markdown again',
+      createdAt: '2019-10-05T10:12:44.007Z',
+      updatedAt: '2019-10-05T10:13:15.237Z',
+      tagList: [],
+      description: 'me',
+      author: { username: 'Bennett', bio: null, image: 'https://static.productionready.io/images/smiley-cyrus.jpg', following: false },
+      favorited: false,
+      favoritesCount: 6
+    }
+  ];
