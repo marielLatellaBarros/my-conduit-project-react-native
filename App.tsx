a877d9dc0aa61ee5741ff0a5168e8aca5944ea3d
Mariel Latella Barros <mariel.latella@gmail.com>
2020-11-17 23:33:59 +0100

    Implement FlatList
    A performant interface for rendering basic, flat lists, supporting the most handy features:
    
    Fully cross-platform.
    Optional horizontal mode.
    Configurable viewability callbacks.
    Header support.
    Footer support.
    Separator support.
    Pull to Refresh.
    Scroll loading.
    ScrollToIndex support.
    Multiple column support.
    
    By passing extraData={selected} to FlatList we make sure FlatList itself will re-render when the state changes.
    Without setting this prop, FlatList would not know it needs to re-render any items because it is a PureComponent and the prop comparison will not show any changes.
    keyExtractor tells the list to use the ids for the react keys instead of the default key property.
    
    renderItem#
    renderItem({ item, index, separators });
    Takes an item from data and renders it into the list.
    item (Object): The item from data being rendered.
    index (number): The index corresponding to this item in the data array.
    separators (Object)
    highlight (Function)
    unhighlight (Function)
    updateProps (Function)
    select (enum('leading', 'trailing'))
    newProps (Object)
    
    data#
    For simplicity, data is a plain array. If you want to use something else, like an immutable list, use the underlying VirtualizedList directly.
    
    ItemSeparatorComponent#
    Rendered in between each item, but not at the top or bottom. By default, highlighted and leadingItem props are provided. renderItem provides separators.highlight/unhighlight which will update the highlighted prop, but you can also add custom props with separators.updateProps.
    
    keyExtractor#
    (item: object, index: number) => string;
    Used to extract a unique key for a given item at the specified index. Key is used for caching and as the react key to track item re-ordering. The default extractor checks item.key, then falls back to using the index, like React does.
    
    more info: https://reactnative.dev/docs/flatlist

--- App.tsx
+++ App.tsx
@@ -1,13 +1,50 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import { ArticlePreview } from './src/ui/article/ArticlePreview';
 import { ARTICLES } from './assets/articles';
 
+
+type Article = {
+  title: string;
+  slug: string;
+  body: string;
+  createdAt: string;
+  updatedAt: string;
+  tagList: string[];
+  description: string;
+  author: Author;
+  favorited: boolean;
+  favoritesCount: number;
+};
+
+type Author = {
+  username: string;
+  bio?: string;
+  image?: string;
+  following: boolean;
+};
+
 export default function App() {
-  const articles = ARTICLES;
+  const articles: Article[] = ARTICLES;
+  const renderItem = ({ item }: { item: Article }): JSX.Element => {
+    return (
+      <View style={{margin: 8}}>
+        <ArticlePreview {...item} />
+      </View>
+    );
+  };
+
+  const RenderSeparator = () => <View style={{ marginHorizontal: 8, borderBottomWidth: 1, borderBottomColor: '#CCC' }}></View>;
+
+
   return (
-     <View style={styles.container}>
-            <ArticlePreview {...articles[0]} />
+    <View style={styles.container}>
+      <FlatList 
+      data={articles} 
+      renderItem={renderItem} 
+      ItemSeparatorComponent={RenderSeparator} 
+      keyExtractor={article => article.slug} 
+      />
     </View>
   );
 }
@@ -20,3 +57,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   }
 });
+
+
